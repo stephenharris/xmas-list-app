506f980b9e7b641651811c73fc24a316ec23c693
Stephen Harris <contact@stephenharris.info>
2019-12-14 23:43:10 +0000

    improve sign up page on mobile

--- src/SignUp.js
+++ src/SignUp.js
@@ -16,17 +16,18 @@ function SignUp() {
         <ol>
           <li>
             <strong>Log-in</strong>
-            with Google or via email. No need to set a password
+            <div>with Google or via email. No need to set a password
               <LoginWithEmailForm/>
               <p>or <GoogleLoginButton/></p>
+            </div>
           </li>
           <li>
             <strong>Create a list</strong>
-            Add items to your wishlist
+            <div>Add items to your wishlist</div>
           </li>
           <li>
             <strong>Share your list</strong>
-            with friends and family. They can mark the ones they plan to get you, and other's will see those items crossed-out. 
+            <div>with friends and family. They can mark the ones they plan to get you, and other's will see those items crossed-out. </div>
           </li>
         </ol>
       </div>
