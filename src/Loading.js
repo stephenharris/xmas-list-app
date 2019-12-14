ddbd04f9c14bcb0f2b756ead8c883b23a3734728
Stephen Harris <contact@stephenharris.info>
2019-12-14 20:53:08 +0000

    Fix className property

--- src/Loading.js
+++ src/Loading.js
@@ -5,25 +5,25 @@ function Loading() {
 
   return (
     <div id="loading">
-        <div class="ball-wrapper">
-            <div class="ring"></div>
-            <div class="button"></div>
-            <div class="red-ball"></div>
+        <div className="ball-wrapper">
+            <div className="ring"></div>
+            <div className="button"></div>
+            <div className="red-ball"></div>
         </div>
-        <div class="ball-wrapper">
-            <div class="ring"></div>
-            <div class="button"></div>
-            <div class="green-ball"></div>
+        <div className="ball-wrapper">
+            <div className="ring"></div>
+            <div className="button"></div>
+            <div className="green-ball"></div>
         </div> 
-        <div class="ball-wrapper">
-            <div class="ring"></div>
-            <div class="button"></div>
-            <div class="red-ball"></div>
+        <div className="ball-wrapper">
+            <div className="ring"></div>
+            <div className="button"></div>
+            <div className="red-ball"></div>
         </div> 
-        <div class="ball-wrapper">
-            <div class="ring"></div>
-            <div class="button"></div>
-            <div class="green-ball"></div>
+        <div className="ball-wrapper">
+            <div className="ring"></div>
+            <div className="button"></div>
+            <div className="green-ball"></div>
         </div>
     </div>
   );
