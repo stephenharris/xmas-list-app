145d358d437f89f1f1b9ddd7c360ab4d40356851
Stephen Harris <contact@stephenharris.info>
2020-10-26 13:21:27 +0000

    log auth0 user

--- src/Auth0LoginButton.js
+++ src/Auth0LoginButton.js
@@ -3,10 +3,11 @@ import { useAuth0 } from "@auth0/auth0-react";
 import Button from './Button';
 
 const Auth0LoginButton = ({match}) => {
-  const { loginWithRedirect } = useAuth0();
+  const { user, loginWithRedirect } = useAuth0();
 
   const alpha = localStorage.getItem('alpha');
   console.log(alpha)
+  console.log(user)
 
   return alpha && alpha === "true" ? <Button  onClick={() => loginWithRedirect()}>Log In</Button> : null;
 };
