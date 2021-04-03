cecda693a409dff2973786b91858c2c7af3bd0ba
Stephen Harris <contact@stephenharris.info>
2021-04-03 20:36:29 +0100

    Fix missing file

---
+++ src/EditIcon.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function EditIcon() {
+
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" style={{"width": "2em"}}>
+        <path class="primary" fill="#ffda71" d="M4 14a1 1 0 0 1 .3-.7l11-11a1 1 0 0 1 1.4 0l3 3a1 1 0 0 1 0 1.4l-11 11a1 1 0 0 1-.7.3H5a1 1 0 0 1-1-1v-3z"></path>
+        <rect width="20" fill="#ffb54a" height="2" x="2" y="20" class="secondary" rx="1"></rect>
+    </svg>
+  );
+}
+
+export default EditIcon;
+
